008ec8d411dfe29478d8871afbeafba5c02260f7
Mnf Gl <menafgul@gmail.com>
2016-12-29 12:19:00 +0200

    AWS Message

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,10 +2,47 @@
 
 namespace NotificationChannels\AwsSns\Exceptions;
 
-class CouldNotSendNotification extends \Exception
+use Exception;
+
+class CouldNotSendNotification extends Exception
 {
+    /**
+     * @param \Aws\Sns\Exception $response
+     *
+     * @return static
+     */
+    public static function connectionFailed(Exception $exception)
+    {
+        return new static("Could not connect to the AWS. {$exception->getAwsErrorCode()}: {$exception->getMessage()}");
+    }
+
+    /**
+     * @param \Aws\Sns\Exception $response
+     *
+     * @return static
+     */
+    public static function invalidParameter(Exception $exception)
+    {
+        return new static("A request parameter or its value does not comply with the associated constraints. {$exception->getAwsErrorCode()}: {$exception->getMessage()}");
+    }
+
+    /**
+     * @param \Aws\Sns\Exception $response
+     *
+     * @return static
+     */
+    public static function notAuthorized(Exception $exception)
+    {
+        return new static("The user has been denied to access the requested resource. {$exception->getAwsErrorCode()}: {$exception->getMessage()}");
+    }
+
+    /**
+     * @param \Aws\Sns\Exception $response
+     *
+     * @return static
+     */
     public static function serviceRespondedWithAnError($response)
     {
-        return new static("Descriptive error message.");
+        return new static("Service responded with an error. {$exception->getAwsErrorCode()}: {$exception->getMessage()}");
     }
 }
